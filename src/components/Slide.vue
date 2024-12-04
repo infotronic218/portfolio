650d3103ff58bc82e76f4df130dfbc6f87f6c002
infotronic218 <infotronic218@gmail.com>
2024-12-04 23:33:53 +0100

    Update the portfolio

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -8,14 +8,14 @@
               
            </div>
        </div>
-       <h5 class="mt-3 text-white text-center text-uppercase"><router-link  to="/" class="text-white">Sawadogo Ousseni </router-link></h5>
+       <h5 class="mt-3 text-white text-center text-uppercase"><router-link  to="" class="text-white">Sawadogo Ousseni </router-link></h5>
        <h6 class="text-center text-warning"> <strong>Embedded Electronic and Software Engineer </strong></h6>
        <div class="row justify-content-center mt-5">
            <div class="col-sm-12 mb-3">
               <div class="list-group">
                   <router-link  to="competences" class="list-group-item  m-1 btn btn-primary list-group-item-action" exact-active-class="active"><strong><i class="fa fa-cogs" aria-hidden="true"></i> Competences</strong></router-link>
                   <router-link to="experiences" class="list-group-item btn btn-primary m-1 list-group-item-action" exact-active-class="active"><strong><i class="fa fa-star"></i> Experiences</strong></router-link>
-                  <router-link  to="/projects" class="list-group-item btn btn-primary m-1  list-group-item-action " exact-active-class="active"><strong><i class="fas fa-project-diagram    "></i> Projects</strong></router-link>
+                  <router-link  to="projects" class="list-group-item btn btn-primary m-1  list-group-item-action " exact-active-class="active"><strong><i class="fas fa-project-diagram    "></i> Projects</strong></router-link>
                   <router-link  to="education" class="list-group-item btn btn-primary m-1  list-group-item-action " exact-active-class="active"><strong><i class="fa fa-graduation-cap" aria-hidden="true"></i> Education</strong></router-link>
                   <router-link  to="certificates" class="list-group-item btn btn-primary m-1  list-group-item-action " exact-active-class="active"><strong><i class="fas fa-certificate    "></i> Certificates</strong></router-link>
                   <router-link  to="contact" class="list-group-item btn btn-primary m-1  list-group-item-action " exact-active-class="active"><strong><i class="fa fa-address-card" aria-hidden="true"></i> Contact</strong></router-link>
